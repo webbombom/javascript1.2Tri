cb5cb0dfdd2cd7f11e297a33efc02f799c8b76d5
webbombom <isadora.siqueira.barbosa@escola.pr.gov.br>
2026-03-19 07:44:30 -0300

    create: aula //19032026

--- aulas/13032026.js
+++ aulas/13032026.js
@@ -1,12 +1,33 @@
 
 let lucro= 30
 let lucro2= 10
-function soma(a,b);
-    return (a + b){
-    }
-    soma(lucro,lucro2)
+function soma(a,b){
+    return a + b}
+
+soma(lucro,lucro2)
 
     function resultadoDivisaoSocio(lucro, lucro2){
         return soma(lucro, lucro2)/2
     }
- 
+  function calculadora(ari,a,b){
+  switch(ari){
+    case '+':
+        resultado = a + b;
+        break;
+        case '-':
+            resultado = a + b;
+            case '-':
+                resultado = a - b;
+    break;
+case '*':
+resultado = a * b;
+    break
+case '/':
+    resultado = a / b;
+    break;
+    default:
+        resultado = 'Operador inválido';
+
+}
+return resultado
+}
