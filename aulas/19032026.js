0c772a70eaa08d4ac7ca0876f0d643ca4d1cdf21
webbombom <isadora.siqueira.barbosa@escola.pr.gov.br>
2026-03-20 07:53:42 -0300

    update

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -1,3 +1,15 @@
 let numero =  30
 let decimal = 20.30
 let nome = "Não sei, você sabe?"
+console.log(nome===nome);
+const imputMinusculo = nome.toLowerCase();
+const senha = "minha sena123"
+console.log(senha.length)
+
+let nz = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log(`nv é diferente, pois ele é ${nv}`)
+    
+}
