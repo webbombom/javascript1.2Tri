10c92be5af96f64925e8c4925417af7d51e3ccfd
webbombom <isadora.siqueira.barbosa@escola.pr.gov.br>
2026-03-27 07:57:18 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,23 @@
+//Criar Array
+
+const filaNumber =[1,2,3,4,5,6];
+
+const filaStringAmigos = ["Ana Julia"];
+const filaStringAmigos = ["Isadora"];
+const filaStringAmigos = ["Stevam"];
+const filaStringAmigos = ["Duda"];
+const filaStringAmigos = ["Zé"];
+const filaStringAmigos = ["Mayte"];
+
+function lerAmigos (filaStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length}amigos`)
+    for(let i = 0; i < filaStringAmigos.length;i++){
+            console.log(filaStringAmigos[i]){
+                if(filaStringAmigos[i]=== "Bob"){
+                    console.log(`Index do seu melhor amigo e ${i}`)
+                }
+            }
+            
+    }
+
+}
